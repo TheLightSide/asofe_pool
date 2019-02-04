ecab4fec97022d79871f2f5d48a3d6d363612d8a
develnk <develnk@gmail.com>
2019-02-05 00:35:39 +0300

    Number round

--- website/static/miner_stats.js
+++ website/static/miner_stats.js
@@ -223,7 +223,7 @@ $.getJSON('/api/payouts?'+_miner, function(payouts){
 	var total_amount = 0.0;
 
 	for (let p in payouts) {
-		total_amount += parseFloat(payouts[p].amount.toFixed(2));
+		total_amount += Number(payouts[p].amount);
 		var newRowContent = '<tr><th scope="row">' + i + '</th>';
 		newRowContent+= '<td>' + payouts[p].amount + '</td>';
 		newRowContent+= '<td><a href="' + payouts[p].url + '" target="_blank"><p class="pay-url" >' + payouts[p].url + '</p></a></td>';
@@ -232,7 +232,7 @@ $.getJSON('/api/payouts?'+_miner, function(payouts){
 		i--;
 	}
 
-	$("#totalAmount").append(total_amount);
+	$("#totalAmount").append(total_amount.toFixed(2));
 	$("#totalTransactions").append(payouts.length);
 });
 
